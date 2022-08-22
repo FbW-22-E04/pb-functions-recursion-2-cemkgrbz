9d4ba6c0c7504797dcbf18c558b526b9f6b32586
Cem <cemkgrbz@gmail.com>
2022-08-22 15:41:18 +0200

    added js file

---
+++ index.js
@@ -0,0 +1,31 @@
+//1
+
+
+function factorial(n) {
+    if(n < 0){
+        return -1;}
+    else if(n == 0){
+        return 1;}
+    else{
+        return(n * factorial(n-1))
+    }
+  }
+  console.log(factorial(5));
+
+//recursion first exercise
+
+// Calculate the sum of natural number up to n
+
+//     Write a JavaScript program to compute the sum of an array of integers
+
+
+const array = [1, 2, 3, 4, 5, 6]
+
+function sum(arr) {
+    if (arr.length === 0) {
+        return 0
+    }
+    return arr.pop() + sum(arr);
+}
+
+console.log(sum(array)) // return 21
